f9763a581b8b973c6514893fba492716aba283e5
adinha19 <adin.hadzovic1@gmail.com>
2024-03-20 15:33:08 +0100

    move socket to serverjs

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-const { socketManager } = require("./socket");
-const { stopAllCronJobs } = require("./cron");
+const { Server } = require("socket.io");
+const { stopAllCronJobs, startCronJob, stopCronJob } = require("./cron");
 
 const PORT = process.env.PORT || 3005;
 const app = express();
@@ -11,7 +11,32 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-socketManager(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3004",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("joinRoom", async (roomId) => {
+    console.log(`User joined room ${roomId}`);
+
+    socket.join(roomId);
+
+    startCronJob(roomId, io);
+  });
+
+  socket.on("leaveRoom", (roomId) => {
+    console.log(`User ${socket.id} left room ${roomId}`);
+    socket.leave(roomId);
+    stopCronJob(roomId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
